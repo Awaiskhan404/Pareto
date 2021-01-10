9883c740ea97c3546e111c0282a652bcebfcd7cc
Awaiskhan404 <contact@awaiskhan.com.pk>
2021-01-10 14:01:33 -0500

    changes made

--- home/views.py
+++ home/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from random_words import RandomWords
 # Create your views here.
 def index(request):
-    return render(request,'home/index.html')
+    context={}
+    context['profiles'] = range(1, 5)
+    context['profiless'] = range(6, 11)
+    context['name']="Russell"
+    return render(request,'home/index.html',context)
