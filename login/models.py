9c239978646e324ae816414e7e8e339b9b5c53c7
Awaiskhan404 <contact@awaiskhan.com.pk>
2021-01-15 07:49:38 -0500

    elliot@fsociety

--- login/models.py
+++ login/models.py
@@ -5,13 +5,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class course(models.Model):
-    course_code=models.IntegerField(null=False)
+    course_code=models.IntegerField(null=False,default=0)
     course_title=models.CharField(max_length=50,null=False,default="")
     course_description=models.CharField(max_length=200,null=False,default="")
     
 class students(models.Model):
     course_code=models.ManyToManyField(course)
     review=models.CharField(max_length=700,default="")
-
+    rating=models.IntegerField(default=0)
+    credit_hours=models.IntegerField(default=0)
+    
 class instructor(models.Model):
     pass
+
--- login/urls.py
+++ login/urls.py
@@ -6,6 +6,9 @@ from login import views
 urlpatterns = [
    
     path('login',views.login,name="login"),
-    path('signup',views.signup,name="signup")
+    path('signup',views.signup,name="signup"),
+    path('auth',views.handle_login,name="auth"),
+    path('auth_signup',views.handle_signup,name="auth_signup")
+    
     
 ]
